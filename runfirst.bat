de493e12a8b7fc89f6357cb05fbdf56df3badd4c
dg8806 <dg8806@163.com>
2018-11-13 14:27:35 +0800

    2017

--- runfirst.bat
+++ runfirst.bat
@@ -4,6 +4,9 @@
 echo 添加SFUChat环境变量
 set regpath=HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment
 set evname=SFUChat
-set javapath=%~dp0
-reg add "%regpath%" /v %evname% /d %javapath% /f
+set evnpath=%~dp0
+reg add "%regpath%" /v %evname% /d %evnpath% /f
+set evname=WEBRTC13
+set evnpath=%~dp0ThirdParty\webrtc\src
+reg add "%regpath%" /v %evname% /d %evnpath% /f
 pause>nul
